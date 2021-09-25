04f120a05df5b648fc26026024f4ab28207e224c
mathletedev <mathletedev@gmail.com>
2021-09-24 20:16:57 -0700

    change margin on welcome page

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -3,7 +3,9 @@ import { FC } from "react";
 const Welcome: FC = () => {
 	return (
 		<div className="flex h-screen w-screen">
-			<div className="text-grey m-auto">currently under development...</div>
+			<div className="text-grey my-10 mx-auto">
+				currently under development...
+			</div>
 		</div>
 	);
 };
