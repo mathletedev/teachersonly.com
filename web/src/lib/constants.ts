24498fe308522aa4583fc0bd7424d0d586d0cda3
mathletedev <mathletedev@gmail.com>
2021-10-10 15:48:07 -0700

    graphql start

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const __callbackUrl__ = __prod__
-	? "https://teachersonly.vercel.app/"
-	: "http://localhost:3000/";
+	? "https://teachersonly.vercel.app"
+	: "http://localhost:3000";
+export const __serverUrl__ = __prod__ ? "" : "http://localhost:4000";
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,16 +1,30 @@
+import {
+	ApolloClient,
+	ApolloProvider,
+	HttpLink,
+	InMemoryCache
+} from "@apollo/client";
 import { Provider } from "next-auth/client";
 import { AppProps } from "next/app";
 import { FC } from "react";
 import Layout from "../components/common/Layout";
+import { __serverUrl__ } from "../lib/constants";
 import "../styles/globals.css";
+
+const client = new ApolloClient({
+	link: new HttpLink({ uri: `${__serverUrl__}/graphql` }),
+	cache: new InMemoryCache()
+});
 
 const _App: FC<AppProps> = ({ Component, pageProps }) => {
 	return (
-		<Provider session={pageProps.session}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</Provider>
+		<ApolloProvider client={client}>
+			<Provider session={pageProps.session}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</Provider>
+		</ApolloProvider>
 	);
 };
 
