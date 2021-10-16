6a0500dc7e5af0a1765179222216e5329ed4ce66
mathletedev <mathletedev@gmail.com>
2021-10-15 20:51:56 -0700

    remove trailing slash

--- server/src/lib/constants.ts
+++ server/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const __port__ = process.env.PORT || "4000";
 export const __prod__ = process.env.NODE_ENV === "production";
 export const __clientUrl__ = __prod__
-	? "https://teachersonly.vercel.app/"
-	: "http://localhost:3000/";
+	? "https://teachersonly.vercel.app"
+	: "http://localhost:3000";
