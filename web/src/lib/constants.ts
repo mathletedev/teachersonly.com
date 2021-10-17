08e58bcd6c48c2c15925daa8139c43586a37628b
mathletedev <mathletedev@gmail.com>
2021-10-17 16:23:33 -0700

    set server endpoint

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const __prod__ = process.env.NODE_ENV === "production";
-export const __serverUrl__ = __prod__ ? "" : "http://localhost:4000";
+export const __serverUrl__ = __prod__
+	? "https://teachersonly.herokuapp.com"
+	: "http://localhost:4000";
